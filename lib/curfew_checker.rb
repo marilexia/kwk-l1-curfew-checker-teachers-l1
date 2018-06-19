0f24c13e69daecf1869b2693868a004808abbb5c
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-19 01:27:46 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -33,7 +33,9 @@ def complex_curfew_checker(time)
 end
 
 def deluxe_curfew_checker(time)
-  
+  if time == 11
+    "Time to apparate!"
+  end
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
