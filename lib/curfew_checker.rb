34834a002b28c3683fda7e1e2e439856e13bcb9b
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-19 00:55:24 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -3,6 +3,8 @@ def simple_curfew_checker(time)
 
   if time == 11
     puts "You're in trouble! Better get home quick!"
+  elsif time > 11
+    puts "You're in trouble! Better get home quick!"
   end
 end
 
