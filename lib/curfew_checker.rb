b30832419e5218cf036301521781526acedd726e
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-19 01:01:02 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,6 +1,4 @@
 def simple_curfew_checker(time)
-  # code goes here
-
   if time >= 11
     "You're in trouble! Better get home quick!"
   elsif
@@ -9,7 +7,10 @@ def simple_curfew_checker(time)
 end
 
 def curfew_checker(time)
-  # code goes here
+  if time = 11
+    "You're in trouble! Better get home quick!"
+
+  
 end
 
 def complex_curfew_checker(time)
